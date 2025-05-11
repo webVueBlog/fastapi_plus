81f3d8a72af8542d4504592432d7f57d5e2e6e44
webVueBlog <2979986590@qq.com>
2025-05-12 07:52:30 +0800

    迭代

--- README.md
+++ README.md
@@ -46,3 +46,5 @@
 
 
 ![img.png](./img.png)
+
+![img_1.png](./img_1.png)
--- fastapi_plus/service/sys_info.py
+++ fastapi_plus/service/sys_info.py
@@ -7,12 +7,14 @@ class SysInfoService(object):
     获取服务运行系统的基础信息
     """
 
+    # 定义一个静态方法，用于获取系统信息
     @staticmethod
     def get_sys_info():
+        # 返回一个字典，包含系统信息
         return {
-            'platform': platform.platform(),
-            'machine': platform.machine(),
-            'node': platform.node(),
-            'processor': platform.processor(),
-            'python_version': platform.python_version(),
+            'platform': platform.platform(),  # 获取操作系统名称及版本号
+            'machine': platform.machine(),  # 获取计算机类型
+            'node': platform.node(),  # 获取计算机的网络名称
+            'processor': platform.processor(),  # 获取计算机处理器信息
+            'python_version': platform.python_version(),  # 获取Python解释器的版本
         }
